a51a63819f625908824fc146772131a690e9cba5
leomir-pereira-dos-santos <leomir101@gmail.com>
2022-10-17 19:13:38 -0300

    adicionado filtro de busca

--- pessoa/views.py
+++ pessoa/views.py
@@ -6,6 +6,14 @@ from .forms import PessoaForm
 class ListaPessoaView(ListView):
     model = Pessoa
     queryset = Pessoa.objects.all().order_by('nome_completo')
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        filtro_nome = self.request.GET.get('nome') or None 
+        
+        if filtro_nome:
+            queryset = queryset.filter(nome_completo__contains=filtro_nome)
+        return queryset
   
 class PessoaCreateView(CreateView):
     model = Pessoa
